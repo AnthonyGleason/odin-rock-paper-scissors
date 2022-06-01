60c0da1ce19f4692f30979f0e008278dc7efeb5e
Anthony Infortunio <ainfortunio1@gmail.com>
2022-06-01 05:42:53 -0400

    Change 2+1 to 3 in computerPlay() function

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 /* This function computerPlay generates the computer choice for the round.
-            A random number between the numbers 1-3 is generated and then rock, paper or
-            scisscors is assigned and returned.*/
-            
-            let computerPlay = function() {
-                let generatedNumber = Math.floor(Math.random()*(2+1)+1);
-                switch (generatedNumber){
-                    case 1:
-                        computerChoice="rock";
-                        break;
-                    case 2:
-                        computerChoice="paper";
-                        break;
-                    case 3:
-                        computerChoice="scissors";
-                        break;
-                }
-                return computerChoice;
-            }
+ A random number between the numbers 1-3 is generated and then rock, paper or
+scisscors is assigned and returned.*/
+
+let computerPlay = function() {
+    let generatedNumber = Math.floor(Math.random()*3+1);
+    switch (generatedNumber){
+        case 1:
+            computerChoice="rock";
+            break;
+        case 2:
+            computerChoice="paper";
+            break;
+        case 3:
+            computerChoice="scissors";
+            break;
+    }
+ return computerChoice;
+}
